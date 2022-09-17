0393f7d70706e7543ba2d9eba216ad3576df9cbf
youyoung00 <you889@hanmail.net>
2022-09-18 00:57:44 +0900

    Riverpod 상태관리의 카운터앱

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // is not restarted.
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+final counterProvider = StateProvider((ref) => 0);
+
+class Home extends ConsumerWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter example'),
+      ),
+      body: Center(
+        child: Consumer(
+          builder: (BuildContext context, WidgetRef ref, Widget? child) {
+            final count = ref.watch(counterProvider.state).state;
+            return Text(
+              '$count',
+              style: const TextStyle(
+                fontSize: 32,
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ref.read(counterProvider.state).state++,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
